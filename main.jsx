58bbf2ea7086f1b204c4bcedef3c986bbf6bed32
vansh21827 <vansh21827@gmail.com>
2026-07-19 20:22:40 +0530

    Create Main Files for accessing Site

---
+++ main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import "./index.css";
+import "./styles/global.css";
+
+import { store } from "./app/store";
+import AuthProvider from "./context/AuthContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
